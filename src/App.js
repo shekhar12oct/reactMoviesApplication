13294adfc6b0c55c74953a3145707f32dcaaf45e
dubey <you@example.com>
2021-03-28 22:58:51 +0530

    New changesw configured

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     results: [],
     selected: {}
   });
-  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
+  const apiurl = "https://www.omdbapi.com/?i=tt3896198&apikey=ad385528";
 
   const search = (e) => {
     if (e.key === "Enter") {
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Results from './Results'
 
 function Result({ result, openPopup }) {
 	return (
 		<div className="result" onClick={() => openPopup(result.imdbID)}>
-			<img src={result.Poster} />
+			<img src={result.Poster} alt="" />
 			<h3>{result.Title}</h3>
 		</div>
 	)
